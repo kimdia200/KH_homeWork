847408645fcc25fee08f6f7d183b57b2dea15713
kimyunsu <kimdia2000@gmail.com>
2020-12-31 22:22:36 +0900

    1231 Object IO Stream update

--- 1231/src/com/io/test4/controller/BookManager.java
+++ 1231/src/com/io/test4/controller/BookManager.java
@@ -22,12 +22,21 @@ public class BookManager {
 	public void fileSave() {
 		//1.객체배열 선언 5개 초기화함 , 샘플데이터 임의작성
 		Book[] b = new Book[5];
-		Calendar cal = Calendar.getInstance();
-		b[0] = new Book("아기돼지 삼형제", "모름", 12_000, cal, 10.0);
-		b[1] = new Book("겨울왕국", "디즈니", 20_000, cal, 5.0);
-		b[2] = new Book("부자아빠", "기요사키 로버트", 15_800, cal, 15.0);
-		b[3] = new Book("부의 추월차선", "엠제이 드마코", 13_500, cal, 8.0);
-		b[4] = new Book("집에있던 낡은책", "몰라몰라", 1_000, cal, 50.0);
+		Calendar cal1 = Calendar.getInstance();
+		Calendar cal2 = Calendar.getInstance();
+		Calendar cal3 = Calendar.getInstance();
+		Calendar cal4 = Calendar.getInstance();
+		Calendar cal5 = Calendar.getInstance();
+		cal1.set(1900, 0,1);
+		cal2.set(2010, 8,6);
+		cal3.set(1990, 0,20);
+		cal4.set(2018, 5,18);
+		cal5.set(1800, 3,1);
+		b[0] = new Book("아기돼지 삼형제", "모름", 12_000, cal1, 10.0);
+		b[1] = new Book("겨울왕국", "디즈니", 20_000, cal2, 5.0);
+		b[2] = new Book("부자아빠", "기요사키 로버트", 15_800, cal3, 15.0);
+		b[3] = new Book("부의 추월차선", "엠제이 드마코", 13_500, cal4, 8.0);
+		b[4] = new Book("집에있던 낡은책", "몰라몰라", 1_000, cal5, 50.0);
 		
 		File f = new File("books.dat");
 		try(
