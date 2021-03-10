d2138228de6246635bf2381a2749657bb3fc3693
kimyunsu <kimdia2000@gmail.com>
2021-03-10 22:17:35 +0900

    [WEB] 210310HW JS

---
+++ web_client_HomeWork/03_JS_HOMEWORK/test.js
@@ -0,0 +1,19 @@
+document.querySelector('#btn-generate').onclick = function(){
+  var pets = [
+    {
+      name: "구리구리",
+      breed: "푸들",
+      weight: 4.5,
+    },
+    {
+      name: "까망이",
+      breed: "도베르만",
+      weight: 15.5,
+    },
+    {
+      name: "하양이",
+      breed: "비숑",
+      weight: 2.5,
+    },
+  ];
+}
